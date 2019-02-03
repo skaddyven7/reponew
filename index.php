f541185a79153877dc62ee026131a39e3995af45
skaddyven7 <47302490+skaddyven7@users.noreply.github.com>
2019-02-03 21:00:07 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Ankieta</title>
+</head>
+
+<body>
+	
+	Witamy w naszej ankiecie!<br /><br />
+	Oto pytania:<br /><br />
+	
+	<form action="checkbox-form.php" method="post">
+	
+	1. Czy często korzystasz z portali społecznościowych?
+
+	
+  <label><input type="radio" name="ankieta" value="Tak"/> tak</label>
+
+  <label><input type="radio" name="ankieta" value="Tak" /> nie</label>
+  
+  <br /><br />
+  
+	<form action="checkbox-form.php" method="post">
+	
+	2. Z jakich najczęściej?
+	
+	<label><input type="radio" name="ankieta" value="Tak"/> Facebook</label>
+
+	<label><input type="radio" name="ankieta" value="Tak" /> Instagram</label>
+
+	<label><input type="radio" name="ankieta" value="Tak" /> Twitter</label>
+	
+	
+	<br /><br />
+	
+	<form action="checkbox-form.php" method="post">
+	
+	3. Ile czasu (dziennie) spędzasz na portalach internetowych?
+	
+	<label><input type="radio" name="ankieta" value="Tak"/> Mniej niż 2 godziny</label>
+
+	<label><input type="radio" name="ankieta" value="Tak" /> Więcej niż 2 godziny</label>
+	
+</form>
+
+</body>
+</html>
+
+
